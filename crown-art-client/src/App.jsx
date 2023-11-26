517db8bdae9d3f8600f4d756db4da3b25b253ed5
Sharmin Jahan Shila <shila2519@gmail.com>
2023-11-27 02:08:37 +0600

    project initialized

---
+++ crown-art-client/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-cyan-300 bg-secondary">Hello Crown Art!</h1>
+    </div>
+  );
+};
+
+export default App;
