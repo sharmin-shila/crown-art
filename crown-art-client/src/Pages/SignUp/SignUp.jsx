46d40a39fbd12b1082b62eecfd8fd6a7c67851dc
Sharmin Jahan Shila <shila2519@gmail.com>
2023-11-30 23:06:32 +0600

    added SignUp component

---
+++ crown-art-client/src/Pages/SignUp/SignUp.jsx
@@ -0,0 +1,130 @@
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import signUpImg from "../../assets/signup/signup.png";
+
+const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  return (
+    <>
+      <div className="hero min-h-screen bg-base-100">
+        <div className="hero-content flex-col md:flex-row-reverse">
+          <div className="text-center md:w-1/2">
+            <img src={signUpImg} alt="sign up image" className="w-2/3 m-auto" />
+          </div>
+          <div className="card md:w-1/2 max-w-sm shadow-2xl bg-base-200">
+            <div className="card-body">
+              <h1 className="text-3xl font-bold text-center text-[#90c641e6]">
+                Sign Up now!
+              </h1>
+              <form onSubmit={handleSubmit()}>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text font-medium">Name</span>
+                  </label>
+                  <input
+                    type="text"
+                    {...register("name", { required: true })}
+                    placeholder="Name"
+                    className="input input-bordered"
+                  />
+                  {errors.name && (
+                    <span className="text-red-600 py-2">Name is required</span>
+                  )}
+                </div>
+
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text font-medium">Email</span>
+                  </label>
+                  <input
+                    type="email"
+                    {...register("email", { required: true })}
+                    placeholder="Email"
+                    className="input input-bordered"
+                  />
+                  {errors.email && (
+                    <span className="text-red-600 py-2">Email is required</span>
+                  )}
+                </div>
+
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text font-medium">Password</span>
+                  </label>
+                  <div className="relative">
+                    <input
+                      type="password"
+                      placeholder="Password"
+                      {...register("password", {
+                        required: true,
+                        minLength: 6,
+                        pattern: /(?=.*[A-Z])(?=.*[!@#$&*])/,
+                      })}
+                      className="input input-bordered w-full"
+                    />
+                  </div>
+                  {errors.password?.type === "required" && (
+                    <span className="text-red-600 py-2">
+                      Password is required
+                    </span>
+                  )}
+                  {errors.password?.type === "minLength" && (
+                    <span className="text-red-600 py-2">
+                      Password must be 6 characters
+                    </span>
+                  )}
+                  {errors.password?.type === "pattern" && (
+                    <span className="text-red-600 py-2">
+                      Password must have one uppercase, one special character
+                    </span>
+                  )}
+                </div>
+
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text font-medium">Upload Photo</span>
+                  </label>
+                  <input
+                    type="file"
+                    accept="image/*"
+                    {...register("photo", { required: true })}
+                    className="file-input file-input-bordered file-input-ghost w-full"
+                  />
+                  {errors.photo && (
+                    <span className="text-red-600 py-2">Photo is required</span>
+                  )}
+                </div>
+
+                <div className="form-control mt-6">
+                  <input
+                    type="submit"
+                    value="Sign Up"
+                    className="btn bg-[#90c641e6] hover:btn-info hover:text-white text-white"
+                  />
+                </div>
+              </form>
+
+              <p>
+                <small>
+                  Already have an account?{" "}
+                  <Link to="/login">
+                    <span className="text-warning font-semibold">
+                      Go to Login
+                    </span>
+                  </Link>
+                </small>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SignUp;
--- crown-art-client/src/Routers/Routes.jsx
+++ crown-art-client/src/Routers/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../Layouts/App";
 import Home from "../Pages/Home/Home/Home";
 import Login from "../Pages/Login/Login";
+import SignUp from "../Pages/SignUp/SignUp";
 
 const router = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
       {
         path: "/login",
         element: <Login />,
+      },
+      {
+        path: "/sign-up",
+        element: <SignUp />,
       },
     ],
   },
