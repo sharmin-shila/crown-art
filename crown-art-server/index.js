1253b14a04c79316e01edd5c7e18b497bf25dda4
Sharmin Jahan Shila <shila2519@gmail.com>
2023-12-04 18:18:33 +0600

    established mongodb connection

--- crown-art-server/index.js
+++ crown-art-server/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,9 +11,36 @@ app.use(cors());
 app.use(express());
 
 app.get("/", (req, res) => {
-  res.send("server is running");
+  res.send("Crown Art is running");
 });
 
 app.listen(port, () => {
-  console.log(`server is running on port: ${port}`);
+  console.log(`Crown Art is running on port: ${port}`);
 });
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.opcngli.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
