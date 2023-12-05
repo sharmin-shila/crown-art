1f55ce433ff2cd448486dc691aae99dc0c4f7c52
Sharmin Jahan Shila <shila2519@gmail.com>
2023-12-06 01:08:42 +0600

    added admin role patch api

--- crown-art-server/index.js
+++ crown-art-server/index.js
@@ -130,6 +130,19 @@ async function run() {
       const result = await usersCollection.updateOne(query, updateDoc);
       res.send(result);
     });
+
+    app.patch("/users/admin/:id", verifyJWT, async (req, res) => {
+      const query = { _id: new ObjectId(req.params.id) };
+
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+
+      const result = await usersCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
