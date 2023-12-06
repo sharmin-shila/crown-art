4e2113188c602a76e4dda5324ea2adbd96579e1f
Sharmin Jahan Shila <shila2519@gmail.com>
2023-12-06 20:49:10 +0600

    added useInstructor hook

---
+++ crown-art-client/src/Hooks/useInstructor/useInstructor.jsx
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../useAuth/useAuth";
+import useAxiosSecure from "../useAxiosSecure/useAxiosSecure";
+
+const useInstructor = () => {
+  const { user, loading } = useAuth();
+
+  const [axiosSecure] = useAxiosSecure();
+
+  const { data: isInstructor, isLoading: isInstructorLoading } = useQuery({
+    queryKey: ["isInstructor", user?.email],
+    enabled: !loading,
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/users/instructor/${user?.email}`);
+      return res.data.instructor;
+    },
+  });
+
+  return { isInstructor, isInstructorLoading };
+};
+
+export default useInstructor;
