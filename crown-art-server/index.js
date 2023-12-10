3ec2eab0fba7a3d83bb31488f59f5804724b5c46
Sharmin Jahan Shila <shila2519@gmail.com>
2023-12-11 01:03:44 +0600

    added course status(denied) patch api

--- crown-art-server/index.js
+++ crown-art-server/index.js
@@ -276,6 +276,26 @@ async function run() {
         res.send(result);
       }
     );
+
+    app.patch(
+      "/courses/admin/deny/:id",
+      verifyJWT,
+      verifyAdmin,
+      async (req, res) => {
+        const id = req.params.id;
+
+        const filter = { _id: new ObjectId(id) };
+
+        const updateDoc = {
+          $set: {
+            status: "denied",
+          },
+        };
+
+        const result = await coursesCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      }
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
